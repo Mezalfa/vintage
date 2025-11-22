cf1b36b6cc15d70ce2699ba3209bee02c9ba6cc8
Mezalfa <65236232+Mezalfa@users.noreply.github.com>
2025-11-22 14:53:10 -0800

    Add Navbar component with navigation links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,42 @@
 
+import { NavLink } from 'react-router-dom';
+
+export default function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top shadow-sm">
+            <div className="container-fluid">
+                <NavLink className="navbar-brand fw-semibold" to="/">VintageVision</NavLink>
+                <button 
+                    className="navbar-toggler" 
+                    type="button" 
+                    data-bs-toggle="collapse" 
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/">Home</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/about">About</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/contact">Contact</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/signin">Sign In</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/upload">Upload</NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
